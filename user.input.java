71f9e8d0b8cbfc1b6593f46b9f8420a450e5c9a5
siddhigodshelwarbatch2024-25 <siddhigodshelwar1404@gmail.com>
2026-04-20 13:52:09 +0530

    Create user.input.java

---
+++ user.input.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class userinput {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter Name : ");
+        String name = sc.nextLine();
+        System.out.println("Name is : " + name);
+
+        System.out.println("Enter age : ");
+        int age = sc.nextInt();
+        System.out.println("Age is : " + age);
+
+        System.out.println("Enter Salary : ");
+        double salary = sc.nextDouble();
+        System.out.println("Salary is : " + salary);
+    }
+}
